b16117d7c893525895dc100dac4d5f8d5e834864
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-07-01 14:12:04 -0500

    Read de los eventos.

--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,15 @@
 const { response } = require('express');
 const Evento = require('../models/Evento');
 
-const getEventos = ( req, res = response ) => {
+const getEventos = async ( req, res = response ) => {
+
+    const eventos = await Evento.find()
+                                .populate('user', 'name');
+    
     return res.json({
         ok: true,
-        msg: 'getEventos'
-    })    
+        eventos
+    });
 }
 
 const crearEvento = async ( req, res = response ) => {
