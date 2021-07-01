b6561a60ecc587148183623f61716e771b6b61be
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-07-01 12:16:49 -0500

    Validación de campos para la creación de eventos.

---
+++ helpers/isDate.js
@@ -0,0 +1,18 @@
+const moment = require('moment');
+
+const isDate = ( value ) => {
+
+    if ( !value ) {
+        return false;
+    }
+
+    const fecha = moment( value );
+    if ( !fecha.isValid()  ) {
+        return false
+    } else {
+        return true;
+    }
+    
+};
+
+module.exports = { isDate };
--- routes/events.js
+++ routes/events.js
@@ -6,14 +6,13 @@
 */
 
 const { Router } = require('express');
-// const { check } = require('express-validator');
-const {
-    getEventos,
-    crearEvento,
-    actualizarEvento,
-    eliminarEvento
-} = require('../controllers/events');
+const { check } = require('express-validator');
+
+
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -24,7 +23,16 @@ router.use( validarJWT );
 router.get('/', getEventos);
 
 // Crear un nuevo evento
-router.post('/', crearEvento);
+router.post(
+    '/',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de fin es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento
+);
 
 // Actualizar Evento
 router.put('/:id', actualizarEvento);
